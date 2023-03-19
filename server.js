cec0bb5abcdfb41024519a2f022b4bb392f6fdd9
Edmund0 <goziem.ofili@gmail.com>
2023-03-19 16:47:01 -0600

    Cloned

--- server.js
+++ server.js
@@ -1,21 +1,19 @@
 // Setup empty JS object to act as endpoint for all routes
-// Express to run server and routes
+projectData = {};
+
+// Require Express to run server and routes
 
 // Start up an instance of app
 
-/* Dependencies */
 /* Middleware*/
-
 //Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 // Cors for cross origin allowance
 
 // Initialize the main project folder
+app.use(express.static('website'));
 
-// Spin up the server
-// Callback to debug
 
-// Initialize all route with a callback function
-
-// Callback function to complete GET '/all'
-
-// Post Route
+// Setup Server
