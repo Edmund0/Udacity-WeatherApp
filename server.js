eca301dc9ac83a927606b1ce5d4a2c0da77b837d
Edmund0 <goziem.ofili@gmail.com>
2023-03-22 08:06:06 -0600

    Completed Framework

--- server.js
+++ server.js
@@ -1,19 +1,40 @@
 // Setup empty JS object to act as endpoint for all routes
 projectData = {};
 
+
+
+
 // Require Express to run server and routes
+const express = require('express');
 
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
-//Here we are configuring express to use body-parser as middle-ware.
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+    //Here we are configuring express to use body-parser as middle-ware.
+    const bodyParser = require('body-parser');
+    app.use(bodyParser.urlencoded({ extended: false }));
+    app.use(bodyParser.json());
 
-// Cors for cross origin allowance
+    // Cors for cross origin allowance
+    const cors = require('cors');
+    app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
 
 // Setup Server
+port = 8000;
+function listening() {  
+        const host = server.address().address  
+        const port = server.address().port  
+        console.log("Example app listening at http://%s:%s", host, port);
+    }
+
+const server = app.listen(port, listening);
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+  })
+
